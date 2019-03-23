b13316cb5f3e384b5d10c8586d6132ebad190de2
Kevin Lewis <kevin.lewis_22@hotmail.com>
2019-03-23 17:38:10 -0400

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v10.15.3/bin/pm2
 
 cd /var/jobtrakr-api
-/home/ec2-user/.nvm/versions/node/v10.15.3/bin/pm2 restart index.js
+sh pm2 restart ecosystem.config.js --env production
